c471cea01c26e7ffc3a1a68d6f8030e7e589a49f
Francisco Marcano <franco@strety.com>
2025-01-19 21:59:39 -0500

    Move non-mini config to the top

--- config/nvim/plugins/luaconfig.lua
+++ config/nvim/plugins/luaconfig.lua
@@ -1,39 +1,6 @@
 require("pqf").setup()
 require("zen-mode").setup()
 
--------- Mini.nvim - https://github.com/echasnovski/mini.nvim
----- Text Editing
-require("mini.ai").setup()
-require("mini.align").setup()
-require("mini.comment").setup()
-require("mini.operators").setup()
-require("mini.pairs").setup()
-require("mini.splitjoin").setup()
-require("mini.surround").setup()
-require('mini.move').setup()
-
----- Workflow
-require("mini.basics").setup()
-require("mini.diff").setup()
-require("mini.jump").setup()
-require("mini.jump2d").setup()
-require("mini.sessions").setup({ autoread = true })
-
--- Appearance
-require("mini.icons").setup()
-require("mini.statusline").setup()
-
--- Others
-require('mini.fuzzy').setup()
-
-require('mini.hipatterns').setup()
-local indentscope = require('mini.indentscope')
-indentscope.setup({
-  draw = { delay = 0, animation = indentscope.gen_animation.none() },
-  symbol = '▏',
-  options = { try_as_border = true }
-})
-
 -- indent-blankline
 require("ibl").setup {
   scope = { show_start = false },
@@ -58,3 +25,36 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+
+-------- Mini.nvim - https://github.com/echasnovski/mini.nvim
+---- Text Editing
+require("mini.ai").setup()
+require("mini.align").setup()
+require("mini.comment").setup()
+require("mini.operators").setup()
+require("mini.pairs").setup()
+require("mini.splitjoin").setup()
+require("mini.surround").setup()
+require('mini.move').setup()
+
+---- Workflow
+require("mini.basics").setup()
+require("mini.diff").setup()
+require("mini.jump").setup()
+require("mini.jump2d").setup()
+require("mini.sessions").setup({ autoread = true })
+
+-- Appearance
+require("mini.icons").setup()
+require("mini.statusline").setup()
+
+-- Others
+require('mini.fuzzy').setup()
+require('mini.hipatterns').setup()
+local indentscope = require('mini.indentscope')
+indentscope.setup({
+  draw = { delay = 0, animation = indentscope.gen_animation.none() },
+  symbol = '▏',
+  options = { try_as_border = true }
+})
