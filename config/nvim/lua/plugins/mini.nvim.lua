55b5902f8015fad3c241007226b6dddf94fb2030
Francisco Marcano <franco@strety.com>
2025-01-27 10:04:26 -0500

    Introduce mini.nvim.lua

---
+++ config/nvim/lua/plugins/mini.nvim.lua
@@ -0,0 +1,53 @@
+return {
+  -- Text Editing
+  { "echasnovski/mini.ai", opts = {} },
+  { "echasnovski/mini.comment", opts = {} },
+  { "echasnovski/mini.completion", opts = {} },
+  { "echasnovski/mini.extra", opts = {} },
+  { "echasnovski/mini.splitjoin", opts = {} },
+  { "echasnovski/mini.surround", opts = {} },
+  {
+    "echasnovski/mini.pairs",
+    opts = {
+      modes = { insert = true, command = true, terminal = false },
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      skip_ts = { "string" },
+      skip_unbalanced = true,
+      markdown = true,
+    },
+  },
+  -- Workflow
+  { "echasnovski/mini.basics", opts = {} },
+  { "echasnovski/mini.bracketed", opts = {} },
+  { "echasnovski/mini.diff", opts = {} },
+  { "echasnovski/mini.sessions", opts = { autoread = true } },
+  {
+    "echasnovski/mini.pick",
+    opts = {},
+    keys = {
+      { "<leader>ff", function() MiniPick.builtin.files({ tool = "git" }) end, desc = "Find Git Files" },
+      { "<leader>fg", function() MiniPick.builtin.grep_live({ tool = "git" }) end, desc = "Live Grep" },
+      { "<leader>fb", function() MiniPick.builtin.buffers({ tool = "git" }) end, desc = "Buffers" },
+      { "<leader>fr", function() MiniExtra.pickers.oldfiles({ preserve_order = true }) end, desc = "Find Recent" },
+      { "<leader>fh", function() MiniExtra.pickers.hl_groups() end, desc = "Find Highlight Groups" },
+      { "<leader>fd", function() MiniExtra.pickers.diagnostic() end, desc = "Find Diagnostics" },
+    },
+  },
+  -- Appearance
+  {
+    "echasnovski/mini.statusline", 
+    opts = {},
+    init = function()
+      -- Disable mini.statusline in NvimTree
+      local augroup = vim.api.nvim_create_augroup("UserMiniNvim", {})
+      vim.api.nvim_create_autocmd("FileType", {
+        group = augroup,
+        pattern = "NvimTree",
+        callback = function(ev)
+          vim.b.ministatusline_disable = true
+        end,
+      })
+    end,
+  },
+}
+
