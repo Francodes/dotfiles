6018b4e87c1c228d3c83da432e825419b25ca1e2
Francisco Marcano <franco@strety.com>
2025-01-28 15:59:44 -0500

    Fix indent

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -10,8 +10,8 @@ return {
 				keymap = {
 					accept = "<C-j>",
 					accept_word = "<C-l>",
-	         next = "<C-k>",
-	         dismiss = "<C-h>",
+	        next = "<C-k>",
+	        dismiss = "<C-h>",
 				},
 			},
       copilot_node_command = "/Users/franco/.asdf/installs/nodejs/18.19.0/bin/node",
