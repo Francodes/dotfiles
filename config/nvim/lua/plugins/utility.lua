1c67c1825f34be2d989fe920e8d5a52df4bee2eb
Francisco Marcano <franco@strety.com>
2025-01-30 05:53:30 -0500

    Change to using lazygit standalone instead of through lazyvim

--- config/nvim/lua/plugins/utility.lua
+++ config/nvim/lua/plugins/utility.lua
@@ -19,7 +19,6 @@ return {
   {
     "echasnovski/mini.basics",
     opts = {
-      options = { extra_ui = true },
       mappings = { windows = true }
     },
   },
@@ -61,15 +60,12 @@ return {
     lazy = false,
     opts = {
       indent = { enabled = true, animate = { enabled = false } },
-      lazygit = { enabled = true },
       notifier = { enabled = true },
       scope = { enabled = true },
       gitbrowse = { enabled = true },
       zen = { enabled = true },
     },
     keys = {
-      -- Lazygit
-      { "<leader>gg", function() Snacks.lazygit() end, desc = "LazyGit" },
       -- Zen
       { "<leader>uz", function() Snacks.zen() end, desc = "Toggle Zen Mode" },
       { "<leader>uZ", function() Snacks.zen.zoom() end, desc = "Toggle Zoom Mode" },
