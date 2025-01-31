9c42f15d12a6156e44e291451ca5af7716765de9
Francisco Marcano <franco@strety.com>
2025-01-31 11:42:29 -0500

    Make sure NvimTree persists through session

--- config/nvim/plugins/luaconfig.lua
+++ config/nvim/plugins/luaconfig.lua
@@ -108,6 +108,18 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.b.ministatusline_disable = true
   end,
 })
+-- Restore NvimTree through sessions
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  pattern = 'NvimTree*',
+  callback = function()
+    local api = require('nvim-tree.api')
+    local view = require('nvim-tree.view')
+
+    if not view.is_visible() then
+      api.tree.open()
+    end
+  end,
+})
 
 require("copilot").setup({
   suggestion = {
