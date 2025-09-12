38bfd299abf54202936bce9ce4577bcf0d29976e
Francisco Marcano <franco@strety.com>
2025-09-12 09:57:24 -0400

    Tweaks

--- config/nvim/plugins/luaconfig.lua
+++ config/nvim/plugins/luaconfig.lua
@@ -12,7 +12,7 @@ require("gruvbox").setup({
     NormalFloat = { fg = "#EBDBB2", bg = "#1B1B1B" },
     StatusLine = { fg = "#1B1B1B", bg = "#1B1B1B" },
     StatusLineNC = { fg = "#1B1B1B", bg = "#1B1B1B" },
-    MiniStatuslineInactive = { fg = "#8E8070", bg = "#1B1B1B"  },
+    MiniStatuslineInactive = { fg = "#8E8070", bg = "#1B1B1B" },
     MiniStatuslineFilename = { fg = "#EBDBB2", bg = "#1B1B1B" },
     MiniStatuslineDevinfo = { fg = "#8E8070", bg = "#1B1B1B" },
     MiniStatuslineFileinfo = { fg = "#8E8070", bg = "#1B1B1B" },
@@ -128,8 +128,8 @@ require("copilot").setup({
     keymap = {
       accept = "<C-j>",
       accept_word = "<C-l>",
-       next = "<C-k>",
-       dismiss = "<C-h>",
+      next = "<C-k>",
+      dismiss = "<C-h>",
     },
   },
 })
