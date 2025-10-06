9fa221450cf427787c148c3e1e0a1193705cba88
Francisco Marcano <franco@strety.com>
2025-10-06 06:35:22 -0400

    Update lazy-nvim

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -1,8 +1,3 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		opts = {
-			copilot_node_command = "/Users/franco/.asdf/installs/nodejs/18.19.0/bin/node",
-		},
-	},
+	{ "zbirenbaum/copilot.lua" },
 }
