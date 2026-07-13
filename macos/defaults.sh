c4d758c4dc8b4d67ca3c0ad9387b4e7124c43c91
Francisco Marcano <franco@strety.com>
2026-07-13 17:18:08 -0400

    Disable Quick Note hot corner

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -7,7 +7,6 @@ defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock orientation -string "left"
-defaults write com.apple.dock wvous-br-corner -int 14
 
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
