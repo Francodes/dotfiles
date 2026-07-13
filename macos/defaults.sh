fb4bc5780ba9101de5b23f0dccfceac08dad59f2
Francisco Marcano <franco@strety.com>
2026-07-13 17:18:22 -0400

    Pin bottom-right hot corner off (Quick Note is the macOS default)

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -7,6 +7,7 @@ defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock orientation -string "left"
+defaults write com.apple.dock wvous-br-corner -int 1
 
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
